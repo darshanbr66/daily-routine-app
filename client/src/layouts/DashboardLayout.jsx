13435918d2190abdf9a93a292beeffaccd679185
SanthoshkumarGudi <santhosh@sigvitas.com>
2026-07-07 12:15:05 +0530

    fix: add Vercel rewrite configuration for React Router

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -5,23 +5,21 @@ import Navbar from "../components/layout/Navbar";
 
 function DashboardLayout() {
   return (
-    <div className="min-h-screen bg-slate-100">
-      <div className="flex">
-        {/* Desktop Sidebar */}
-        <aside className="hidden md:block">
-          <Sidebar />
-        </aside>
+    <div className="flex min-h-screen bg-slate-100">
+      {/* Desktop Sidebar */}
+      <aside className="hidden md:block">
+        <Sidebar />
+      </aside>
 
-        {/* Main Section */}
-        <div className="flex min-h-screen flex-1 flex-col">
-          {/* Top Navigation */}
-          <Navbar />
+      {/* Main Section */}
+      <div className="flex min-h-screen flex-1 flex-col">
+        {/* Top Navigation */}
+        <Navbar />
 
-          {/* Page Content */}
-          <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
-            <Outlet />
-          </main>
-        </div>
+        {/* Page Content */}
+        <main className="flex-1 bg-slate-100 p-4 md:p-6 lg:p-8">
+          <Outlet />
+        </main>
       </div>
     </div>
   );
